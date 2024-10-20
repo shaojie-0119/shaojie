91980127d2be9ee90bc6ac469eca169b640f2985
shaojie-0119 <shaojie_0119@163.com>
2024-10-20 10:42:07 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     // 检查账号密码是否正确
     if (username === "root" && password === "123456") {
       // 跳转到系统主页
-      window.location.href = "XITONG.html";
+      window.location.href = "https://www.btbu.edu.cn";
     } else {
       // 提示账号密码不正确
       document.getElementById("errorMessage").textContent = "用户名或密码错误。";
